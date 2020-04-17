cbcd225909ea192f3fccf63854d250c9a3a9bcdf
kri26 <55378432+kri26@users.noreply.github.com>
2020-04-17 16:41:04 +0400

    laba5

---
+++ RepairClientView/FormRegister.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using RepairBusinessLogic.BindingModels;
+
+namespace RepairClientView
+{
+    public partial class FormRegister : Form
+    {
+        public FormRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRegister_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text) &&
+           !string.IsNullOrEmpty(textBox2.Text) &&
+           !string.IsNullOrEmpty(textBox3.Text))
+            {
+                try
+                {
+                    ApiClient.PostRequest("api/client/register", new ClientBindingModel
+                    {
+                        ClientFIO = textBox1.Text,
+                        Login = textBox2.Text,
+                        Password = textBox3.Text
+                    });
+                    MessageBox.Show("Регистрация прошла успешно", "Сообщение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин, пароль и ФИО", "Ошибка",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
