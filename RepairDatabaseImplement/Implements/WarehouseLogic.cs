ae58d43ae7cd94219c599aedff1ca8f9a66a339a
kri26 <55378432+kri26@users.noreply.github.com>
2020-04-17 10:59:45 +0400

    laba3home

--- RepairDatabaseImplement/Implements/WarehouseLogic.cs
+++ RepairDatabaseImplement/Implements/WarehouseLogic.cs
@@ -1,31 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 using RepairBusinessLogic.BindingModels;
 using RepairBusinessLogic.Interfaces;
 using RepairBusinessLogic.ViewModels;
 using RepairDatabaseImplement.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace RepairDatabaseImplement.Implements
 {
     public class WarehouseLogic : IWarehouseLogic
     {
+
         public void CreateOrUpdate(WarehouseBindingModel model)
         {
             using (var context = new RepairDatabase())
             {
                 Warehouse element = context.Warehouses.FirstOrDefault(rec => rec.WarehouseName == model.WarehouseName && rec.Id != model.Id);
-
                 if (element != null)
                 {
-                    throw new Exception("Уже есть склад с таким названием");
+                    throw new Exception("Уже есть изделие с таким названием");
                 }
-
                 if (model.Id.HasValue)
                 {
                     element = context.Warehouses.FirstOrDefault(rec => rec.Id == model.Id);
-
                     if (element == null)
                     {
                         throw new Exception("Элемент не найден");
@@ -36,9 +35,7 @@ namespace RepairDatabaseImplement.Implements
                     element = new Warehouse();
                     context.Warehouses.Add(element);
                 }
-
                 element.WarehouseName = model.WarehouseName;
-
                 context.SaveChanges();
             }
         }
@@ -47,26 +44,74 @@ namespace RepairDatabaseImplement.Implements
         {
             using (var context = new RepairDatabase())
             {
+                context.WarehouseMaterials.RemoveRange(context.WarehouseMaterials.Where(rec => rec.WarehouseId == model.Id));
+                Warehouse element = context.Warehouses.FirstOrDefault(rec => rec.Id == model.Id);
+                if (element != null)
+                {
+                    context.Warehouses.Remove(element);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Элемент не найден");
+                }
+            }
+        }
+
+        public List<WarehouseViewModel> Read(WarehouseBindingModel model)
+        {
+            using (var context = new RepairDatabase())
+            {
+                return context.Warehouses.Where(rec => model == null || rec.Id == model.Id)
+                .ToList()
+                .Select(rec => new WarehouseViewModel
+                {
+                    Id = rec.Id,
+                    WarehouseName = rec.WarehouseName,
+                    WarehouseMaterials = context.WarehouseMaterials.
+                                                Include(recSM => recSM.Material)
+                                                .Where(recSM => recSM.WarehouseId == rec.Id)
+                                                .ToDictionary(recSM => recSM.MaterialId, 
+                                                recSM => (recSM.Material?.MaterialName, recSM.Count))
+                }).ToList();
+            }
+        }
+
+        public void WriteOffMaterials(OrderViewModel order)
+        {
+            using (var context = new RepairDatabase())
+            {
                 using (var transaction = context.Database.BeginTransaction())
                 {
                     try
                     {
-                        context.WarehouseMaterials.RemoveRange(context.WarehouseMaterials.Where(rec => rec.WarehouseId == model.Id));
-                        Warehouse element = context.Warehouses.FirstOrDefault(rec => rec.Id == model.Id);
-
-                        if (element != null)
+                        var repairWorkMaterials = context.RepairWorkMaterials.Where(dm => dm.RepairWorkId == order.RepairWorkId).ToList();
+                        var warehouseMaterials = context.WarehouseMaterials.ToList();
+                        foreach (var material in repairWorkMaterials)
                         {
-                            context.Warehouses.Remove(element);
-                            context.SaveChanges();
+                            var materialCount = material.Count * order.Count;
+                            foreach (var sm in warehouseMaterials)
+                            {
+                                if (sm.MaterialId == material.MaterialId && sm.Count >= materialCount)
+                                {
+                                    sm.Count -= materialCount;
+                                    materialCount = 0;
+                                    context.SaveChanges();
+                                    break;
+                                }
+                                else if (sm.MaterialId == material.MaterialId && sm.Count < materialCount)
+                                {
+                                    materialCount -= sm.Count;
+                                    sm.Count = 0;
+                                    context.SaveChanges();
+                                }
+                            }
+                            if (materialCount > 0)
+                                throw new Exception("Не хватает материалов на складах!");
                         }
-                        else
-                        {
-                            throw new Exception("Элемент не найден");
-                        }
-
                         transaction.Commit();
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         transaction.Rollback();
                         throw;
@@ -79,94 +124,18 @@ namespace RepairDatabaseImplement.Implements
         {
             using (var context = new RepairDatabase())
             {
-                WarehouseMaterial element =
-                    context.WarehouseMaterials.FirstOrDefault(rec => rec.WarehouseId == model.WarehouseId && rec.MaterialId == model.MaterialId);
-
-                if (element != null)
-                {
-                    element.Count += model.Count;
-                }
+                var warehouseMaterial = context.WarehouseMaterials
+                    .FirstOrDefault(sm => sm.MaterialId == model.MaterialId && sm.WarehouseId == model.WarehouseId);
+                if (warehouseMaterial != null)
+                    warehouseMaterial.Count += model.Count;
                 else
-                {
-                    element = new WarehouseMaterial();
-                    context.WarehouseMaterials.Add(element);
-                    element.Count = model.Count;
-                }
-
-                element.WarehouseId = model.WarehouseId;
-                element.MaterialId = model.MaterialId;
-                
-
+                    context.WarehouseMaterials.Add(new WarehouseMaterial()
+                    {
+                        MaterialId = model.MaterialId,
+                        WarehouseId = model.WarehouseId,
+                        Count = model.Count
+                    });
                 context.SaveChanges();
-            }
-        }
-
-        public List<WarehouseViewModel> Read(WarehouseBindingModel model)
-        {
-            using (var context = new RepairDatabase())
-            {
-                return context.Warehouses
-                .Where(rec => model == null || rec.Id == model.Id)
-                .ToList()
-                .Select(rec => new WarehouseViewModel
-                {
-                    Id = rec.Id,
-                    WarehouseName = rec.WarehouseName,
-                    WarehouseMaterials = context.WarehouseMaterials
-                                                .Include(recWC => recWC.Material)
-                                                .Where(recWC => recWC.WarehouseId == rec.Id)
-                                                .ToDictionary(recWC => recWC.MaterialId, recWC => (
-                                                    recWC.Material?.MaterialName, recWC.Count
-                                                ))
-                })
-                .ToList();
-            }
-        }
-
-        public void WriteOffMaterials(OrderViewModel model)
-        {
-            using (var context = new RepairDatabase())
-            {
-                using (var transaction = context.Database.BeginTransaction())
-                {
-                    try
-                    {
-                        var productMaterials = context.RepairWorkMaterials.Where(rec => rec.RepairWorkId == model.RepairWorkId).ToList();
-
-                        foreach (var pc in productMaterials)
-                        {
-                            var warehouseMaterial = context.WarehouseMaterials.Where(rec => rec.MaterialId == pc.MaterialId);
-                            int neededCount = pc.Count * model.Count;
-
-                            foreach (var wc in warehouseMaterial)
-                            {
-                                if (wc.Count >= neededCount)
-                                {
-                                    wc.Count -= neededCount;
-                                    neededCount = 0;
-                                    break;
-                                }
-                                else
-                                {
-                                    neededCount -= wc.Count;
-                                    wc.Count = 0;
-                                }
-                            }
-
-                            if (neededCount > 0)
-                            {
-                                throw new Exception("На складах недостаточно компонентов");
-                            }
-
-                        }
-
-                    }
-                    catch (Exception)
-                    {
-                        transaction.Rollback();
-                        throw;
-                    }
-                }
             }
         }
     }
